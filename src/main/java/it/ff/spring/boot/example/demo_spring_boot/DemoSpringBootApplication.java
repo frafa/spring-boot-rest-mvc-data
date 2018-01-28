91eec960950eb3cacac87cb2e8b21f5927082dd0
Francesco F <francesco.fantoni@gmail.com>
2018-01-28 21:46:55 +0100

    Create prj with SPRING INITIALIZR

---
+++ src/main/java/it/ff/spring/boot/example/demo_spring_boot/DemoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package it.ff.spring.boot.example.demo_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/it/ff/spring/boot/example/demo_spring_boot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package it.ff.spring.boot.example.demo_spring_boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
